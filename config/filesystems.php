dbc43db27b8ed6a0ee9a71cc2a03b1fbad92942d
dddimmmi <mitak.1985@abv.bg>
2017-10-09 14:35:02 +0300

    Laravel-Blog Project
    Laravel Blog with Admin and User panel. Create, Edit, Update and Delete post. Create, Edit, Update and Delete Tag and Category

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,7 +51,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/composer/Bitfumes/storage/app',
+            'url' => env('APP_URL').'/composer/Dikey/storage/app',
             'visibility' => 'public',
         ],
 
